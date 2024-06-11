6afcb922df0e3b7ee3cf2c4353605a8d704a4642
RiskyGUY22 <thomas_turner@talktalk.net>
2024-06-11 20:55:02 +0100

    Update a better impl of the print_board function

--- src/main.c
+++ src/main.c
@@ -1,46 +1,118 @@
 #include <stdio.h>
-#include <wchar.h>
-#include <locale.h>
 
-void print_board() {
+// Define Unicode formmating for the game
+#define RESET   "\033[0m"
+#define BOLD    "\033[1m"
+#define UNDERLINE "\033[4m"
 
-	setlocale(LC_CTYPE, "");
-	wchar_t character = 0X25CC;
+// Define Unicode formmating for the game
+#define RED     "\033[0;31m"
+#define GREEN   "\033[0;32m"
+#define YELLOW  "\033[0;33m"
+#define BLUE    "\033[0;34m"
 
-	for(int i=0; i < 6; i++) {
+void print_board(char board[][7][4]) {
+    // Print column numbers
+    printf("    ");
+    for (int col = 1; col <= 7; col++) {
+        printf("%-4d", col); // Adjusted spacing for alignment
+    }
+    printf("\n");
 
-		for(int j=0; j < 7; j++) {
+    // Print the board
+    for (int row = 0; row < 6; row++) {
+        printf("%-2d", row + 1); // Adjusted spacing for alignment
+        for (int col = 0; col < 7; col++) {
+            printf("| %s ", board[row][col]);
+        }
+        printf("|\n");
+    }
+}
 
-			wprintf(L"%lc", character);
+// Define a struct to hold player data
+struct instance {
+	char player[100];
+	char player_name[4096];
+};
 
-		}
-		wprintf(L"\n");
-	}
+/* The players and their data will be handled by structs, 
+   this functions is used to define that data.*/
+
+void initInstance(struct instance *instance, char *player, char *player_name) {
+	strcpy(instance->player, player);
+	strcpy(instance->player_name, player_name);
+}
+
+// Simple function to get the user's choice from the menu
+int getMenuChoice(int *choice) {
+
+    printf(GREEN"Welcome to my Connect-4 Game!\n\n" RESET);
+    printf(BLUE " 1. Start\n" RESET);
+    printf(YELLOW " 2. Options\n" RESET);
+    printf(RED " 3. Quit\n" RESET);
+
+    printf(BLUE  "\nOPTION: " RESET);
+
+	// Check for invalid inputs
+    if (scanf("%d", choice) != 1) {
+        fprintf(stderr, "Invalid input\n");
+        return 1;
+    }
+
+	// Program has run successfully
+    return 0;
+}
+
+void game_logic() {
+	// Implement game logic here
+
+
 }
 
 int main() {
 
-	int choice;
+    // Define the array to represent the board and initialize it with dotted circles
+	// *NOTE* essentially with Unicode in C, one arguement is worth "4" values so we need to make it a 3D array.
+    char board[6][7][4] = {
+        {"\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C"},
+        {"\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C"},
+        {"\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C"},
+        {"\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C"},
+        {"\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C"},
+        {"\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C", "\xE2\x97\x8C"}
+    };
 
-	printf("Welcome to my Connect-4 Game!\n");
-	printf("1. Start\n");
-	printf("2. Options\n");
-	printf("3. Quit\n");
+	// Initialise player instances
+	struct instance player1;
+	struct instance player2;
 
-	printf("\nOPTION: ");
-	scanf("%d", &choice);
+	// Set player names
+	initInstance(&player1, "Player1", "TT");
+	initInstance(&player2, "Player2", "Computer");
 
+	// Call the menu function to get input from user
+    int choice;
+    getMenuChoice(&choice);
+	printf("\n");
 
-	print_board();
-	if(choice == 1) {
-		print_board();
-	}
-	else if(choice == 2) {
-		printf("Not implemented\n");
-	}
-	else if(choice == 3) {
-		printf("Not implemented\n");
-	}
+	// Handle the user's input
+    switch (choice) {
+        case 1:
+            print_board(board);
+            break;
+        case 2:
+            printf("Options not implemented\n");
+            break;
+        case 3:
+            // Exit program
+			printf(RED "Goodbye!\n" RESET);
+			return 1;
+            break;
+        default:
+            printf("Invalid choice\n");
+            break;
+    }
 
-	return 0;
+	// Return 0 to show that the program has run successfully
+    return 0;
 }
